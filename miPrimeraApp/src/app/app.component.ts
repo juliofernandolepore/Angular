0980194e3663ecfdec612071ee14bce2e52bbad0
FernandoLepore <juliofernandolepore@gmail.com>
2022-10-26 18:08:32 -0300

    manipulando app.component ts hmtl y css

--- miPrimeraApp/src/app/app.component.ts
+++ miPrimeraApp/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'miPrimeraApp';
+  varTitulo = 'contenido de la variable class AppComponent';
+  saludo = "saludo desde angular(segunda variable en AppComponent";
 }
